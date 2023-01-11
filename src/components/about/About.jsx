d1c4d07c438dc030a54517cc45d773ead3cb4626
tristan <tristan.greffe@hotmail.com>
2023-01-11 23:25:42 +0100

    Experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -15,25 +15,25 @@ const About = () => {
     <section id='about'>
       <h5>Pourquoi me choisir ?</h5>
       <h2>À propos de moi</h2>
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
             <img src={ME} alt='About Tristan Greffe' />
           </div>
         </div>
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
               <FaUserGraduate className='about__icon'/>
               <h5>Formations</h5>
               <small>3+ years working</small>
             </article>
-            <article className="about__card">
+            <article className='about__card'>
               <FaAward className='about__icon'/>
               <h5>Expériences</h5>
               <small>3+ years working</small>
             </article>
-            <article className="about__card">
+            <article className='about__card'>
               <GiSkills className='about__icon'/>
               <h5>Skills</h5>
               <small>3+ years working</small>
