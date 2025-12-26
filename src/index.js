a4e274d8b7721464944b76bce5a678dfcb5ec4c7
Tristan <tristan.greffe@hotmail.com>
2025-12-26 15:44:31 +0100

    feat: add theme system with dark/light mode and configurable accent color

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from './context/ThemeContext'
 import App from './App'
 import './scss/index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
